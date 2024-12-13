fa246ee3de3a6188ebc97ce4c69832241dd03bd3
octogradiste <colin.berger@outlook.com>
2024-12-13 12:02:34 +0100

    Add support for validation dataset visualisation

--- visualize.py
+++ visualize.py
@@ -4,14 +4,13 @@ import os
 
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
-from PIL import Image
 
-from src.datasets.data_utils import get_dataloaders
+from src.datasets.image_road_dataset import ImageRoadDataset
 from src.utils.io_utils import ROOT_PATH
-from src.utils.train_utils import reconstruct
+from src.utils.train_utils import create_target
 
 
-def main(image_number):
+def main(image_number, validation=False):
     # Reading submission file
     submission_path = os.path.join(ROOT_PATH, "saved/testing", "submission.csv")
     with open(submission_path, "r") as f:
@@ -35,14 +34,55 @@ def main(image_number):
             h = height // patch_size
             pred_image[h][w] = label
 
-    # Loading image
-    image_path = os.path.join(
-        ROOT_PATH, "road_data", "aicrowd", f"test_{image_number}.png"
-    )
-    image = Image.open(image_path)
+    if validation:
+        print("Using validation dataset")
+        dataset = ImageRoadDataset(
+            os.path.join(ROOT_PATH, "road_data", "validation", "images"),
+            os.path.join(ROOT_PATH, "road_data", "validation", "groundtruth"),
+        )
+    else:
+        print("Using test dataset")
+        dataset = ImageRoadDataset(
+            os.path.join(ROOT_PATH, "road_data", "aicrowd"),
+        )
+
+    image = dataset[image_number]["img"]
+
+    # Transforming image to numpy array
+    image = image.permute(1, 2, 0).numpy()
+
+    # Visualizing the image
     fig, ax = plt.subplots(1)
     ax.imshow(image)
 
+    # Visualizing the mask
+    if validation:
+        mask = dataset[image_number]["mask"]
+
+        # Add a batch dimension and scale to [0, 1]
+        mask = mask.unsqueeze(0) / 255
+
+        # Create the target from the mask, this will be a flattened tensor
+        target = create_target(mask, 16, 0.25).numpy()
+
+        # Reshape the target to the image size, we assume the image is square
+        width = int(target.size**0.5)
+        target = target.reshape(width, width)
+
+        # Visualizing the mask in green
+        for i in range(len(target)):
+            for j in range(len(target[i])):
+                if target[i][j] == 1:
+                    rect = patches.Rectangle(
+                        (j * patch_size, i * patch_size),
+                        patch_size,
+                        patch_size,
+                        facecolor="g",
+                        alpha=0.4,
+                    )
+                    ax.add_patch(rect)
+
+    # Visualizing the predictions in red
     for i in range(len(pred_image)):
         for j in range(len(pred_image[i])):
             if pred_image[i][j] == "1":
@@ -62,6 +102,7 @@ def main(image_number):
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", type=int)
+    ap.add_argument("-v", "--validation", action="store_true")
     args = ap.parse_args()
 
-    main(args.image)
+    main(args.image, args.validation)
