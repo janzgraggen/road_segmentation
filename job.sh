bab3653b67c3afc1ca05a167d37a020ede883c77
octogradiste <colin.berger@outlook.com>
2024-12-19 10:26:17 +0100

    set up training for unets

--- job.sh
+++ job.sh
@@ -14,11 +14,9 @@ git pull
 
 pip3 install -r requirements.txt
 
-python3 train.py -cn exp_transform_noise
-python3 train.py -cn exp_transform_noisy
-python3 train.py -cn exp_arch_mobilenetv3.ymal
-python3 train.py -cn exp_arch_unet_small.ymal
-python3 train.py -cn exp_arch_unet_medium.ymal
-python3 train.py -cn exp_arch_unet_large.ymal
-python3 train.py -cn exp_arch_resnet18.ymal
-python3 train.py -cn exp_arch_resnet36.ymal
+# python3 train.py -cn exp_arch_mobilenetv3
+python3 train.py -cn exp_arch_unet_small
+python3 train.py -cn exp_arch_unet_medium
+python3 train.py -cn exp_arch_unet_large
+# python3 train.py -cn exp_arch_resnet18
+# python3 train.py -cn exp_arch_resnet36
